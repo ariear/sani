86cf0584e78a8b0f7a308829d5e864369a79768e
Arie <91861324+ariear@users.noreply.github.com>
2022-11-15 21:18:44 +0700

    add ui detail anime & change config

--- android/app/src/main/java/com/sani/MainApplication.java
+++ android/app/src/main/java/com/sani/MainApplication.java
@@ -13,6 +13,7 @@ import com.sani.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
+          packages.add(new AsyncStoragePackage());
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
